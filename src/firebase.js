a4b21c267260b02faa7692ccabd42f09297671a7
acolburn <acolburn3@gmail.com>
2026-05-16 10:15:36 -0700

    Added forced long polling for mobile connections

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,5 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Initialize Firestore and export it so we can use it in our components
-export const db = getFirestore(app);
+// 2. Initialize Firestore with forced long-polling configurations
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+});
